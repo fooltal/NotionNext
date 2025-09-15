bd3d61e7c85346b33846bc1a1cb91e08d7eb2631
fooltal <130200032+fooltal@users.noreply.github.com>
2025-09-15 21:17:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '求知学习社', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '求知学习社', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '关注小红书/微信公众号（求知学习社），获得最新动态。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.zhishigongxiang.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
