612a40188a84e32903d41d50c92fde63527fbd27
fooltal <130200032+fooltal@users.noreply.github.com>
2025-09-21 19:55:09 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -10,7 +10,9 @@ export default function PoweredBy(props) {
       <span className='mr-1'>Powered by</span>
       <a
         href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
+        className='underline justify-start'
+        rel='nofollow noopener noreferrer'
+        target='_blank'>
         NotionNext {siteConfig('VERSION')}
       </a>
       .
